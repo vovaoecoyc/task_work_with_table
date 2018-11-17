f7c5f618259e69b2806e01c52edde6990585cb8b
vovaoecoyc <vovaoecoyc@yandex.ru>
2018-11-18 01:45:14 +0300

    after refactoring

--- src/components/table_component.js
+++ src/components/table_component.js
@@ -1,28 +1,13 @@
 'use strict';
 class DataWorker {
   constructor(data = [], rowsOnPage = 5, document) {
-    this.data = this.objectToArray(data);
+    this.data = data;
     this.pageData = [];
     this.rowsOnPage = rowsOnPage;
     this.document = document;
     this.columnsHead = this.document.querySelectorAll('table thead tr th');
     this.tableBody = this.document.querySelector('table.table tbody');
     this.button = this.document.querySelector('input[name=filter]');
-  }
-  objectToArray(data, page = false) {
-    let result = [];
-    if (!page) {
-      for (let i = 0; i < data.length; ++i) {
-        result.push(JSON.parse(JSON.stringify(Object.values(data[i]))));
-      }
-    } else {
-      let pagesActive = this.document.querySelector('.pagination input.active');
-      let a = +pagesActive.name * this.rowsOnPage;
-      for (let i = a; i < this.rowsOnPage; ++i) {
-        result.push(JSON.parse(JSON.stringify(Object.values(data[i]))));
-      }
-    }
-    return result;
   }
 
   fillingTable(data = this.pageData, event = 'filter') {
@@ -38,7 +23,7 @@ class DataWorker {
   fillingRow(dataRow, row) {
     for (let j in dataRow) {
       let td = this.document.createElement('td');
-      td.innerHTML = dataRow[j];
+      td.innerHTML = dataRow[j].search(/\d+,\d+/gi) !== -1 ? `&#36;${dataRow[j]}` : dataRow[j];
       row.appendChild(td);
     }
     return row;
@@ -60,47 +45,37 @@ class DataWorker {
   eventHandlerSort(i, column) {
     let isfilter = this.document.querySelector('table').dataset.isfilter;
     let filter = isfilter === 'filter' ? true : false;
-    let tempDataPage = this.pageData;
     if (filter) {
       this.pageData = this.data;
     }
     let sort = column.dataset.sort;
     for (let j = 0; j < this.pageData.length; ++j) {
       for (let k = j + 1; k < this.pageData.length; ++k) {
-        let temp = this.pageData[j];
-        if (i === this.columnsHead.length - 1) {
-          let left = +this.pageData[j][i].slice(1).replace(/,/gi, '.');
-          let right = +this.pageData[k][i].slice(1).replace(/,/gi, '.');
-          if (sort === '' || sort === 'desc') {
-            if (left > right) {
-              this.pageData[j] = this.pageData[k];
-              this.pageData[k] = temp;
-            }
-          } else if (sort === 'asc') {
-            if (left < right) {
-              this.pageData[j] = this.pageData[k];
-              this.pageData[k] = temp;
-            }
+        let temp = this.pageData[j],
+          leftArg = this.pageData[j][i],
+          rightArg = this.pageData[k][i],
+          reg = /\d+,\d+/gi;
+        if (leftArg.search(reg) !== -1 && rightArg.search(reg) !== -1) {
+          leftArg = Math.floor(+leftArg.replace(/,/gi, '.') * 1000);
+          rightArg = Math.floor(+rightArg.replace(/,/gi, '.') * 1000);
+        }
+        if (sort === '' || sort === 'desc') {
+          if (leftArg > rightArg) {
+            this.pageData[j] = this.pageData[k];
+            this.pageData[k] = temp;
           }
-        } else {
-          if (sort === '' || sort === 'desc') {
-            if (this.pageData[j][i] > this.pageData[k][i]) {
-              this.pageData[j] = this.pageData[k];
-              this.pageData[k] = temp;
-            }
-          } else if (sort === 'asc') {
-            if (this.pageData[j][i] < this.pageData[k][i]) {
-              this.pageData[j] = this.pageData[k];
-              this.pageData[k] = temp;
-            }
+        } else if (sort === 'asc') {
+          if (leftArg < rightArg) {
+            this.pageData[j] = this.pageData[k];
+            this.pageData[k] = temp;
           }
         }
       }
     }
-    this.pageData = tempDataPage;
     column.dataset.sort = sort === '' ? 'asc' : sort === 'asc' ? 'desc' : 'asc';
     let value = this.document.querySelector('input[name=request_string]').value;
-    if (value === '' && filter) {
+    if ((value === '' || value === undefined) && filter) {
+      this.document.querySelector('table').dataset.isfilter = '';
       this.document.querySelector('div.pagination input.active').click();
       return;
     }
@@ -109,31 +84,36 @@ class DataWorker {
   }
 
   eventHandlerFilter() {
-    try {
-      let input = this.document.querySelector('input[name=request_string]');
-      if (input.value !== '') {
-        let filterRows = [];
-        for (let i = 0; i < this.data.length; ++i) {
-          for (let j = 0; j < this.data[i].length; ++j) {
-            let b = this.data[i][j];
-            if (b.toLowerCase().indexOf(input.value.toLowerCase()) !== -1) {
-              filterRows.push(this.data[i]);
-            }
+    let input = this.document.querySelector('input[name=request_string]');
+    if (input.value !== '') {
+      let filterRows = [];
+      for (let i = 0; i < this.data.length; ++i) {
+        //for (let j = 0; j < this.data[i].length; ++j) {
+        for (let j in this.data[i]) {
+          let b = this.data[i][j];
+          if (b.toLowerCase().indexOf(input.value.toLowerCase()) !== -1) {
+            filterRows.push(this.data[i]);
           }
         }
-        if (filterRows.length !== 0) {
-          this.tableBody.innerHTML = '';
-          this.fillingTable(filterRows);
-        }
-      } else {
-        this.tableBody.innerHTML = '';
-        this.fillingTable(this.pageData);
       }
-    } catch (err) {
-      //console.log(err);
+      if (filterRows.length !== 0) {
+        this.tableBody.innerHTML = '';
+        this.fillingTable(filterRows);
+      }
+    } else {
+      this.tableBody.innerHTML = '';
+      this.fillingTable(this.pageData);
     }
   }
-
+  dataForActivePage(data) {
+    let result = [];
+    let pagesActive = this.document.querySelector('.pagination input.active');
+    let index = +pagesActive.name * this.rowsOnPage;
+    for (let i = index; i < this.rowsOnPage; ++i) {
+      result.push(data[i]);
+    }
+    return result;
+  }
   eventHandlerPagination(i) {
     this.document.querySelector('table').dataset.isfilter = '';
     this.document.querySelector('input[name=request_string]').value = '';
@@ -146,7 +126,7 @@ class DataWorker {
         pages[k].classList.add('active');
       }
     }
-    this.pageData = this.objectToArray(this.data, true);
+    this.pageData = this.dataForActivePage(this.data);
 
     let pageNumber = i + 1;
     let rows = [];
@@ -186,7 +166,7 @@ class DataWorker {
       }
       this.document.querySelector('div.pagination').appendChild(buttonPage);
     }
-    this.pageData = this.objectToArray(this.data, true);
+    this.pageData = this.dataForActivePage(this.data);
     this.tablePagination();
   }
 
